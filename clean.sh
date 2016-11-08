4189c12d8be6a7c1f37c64aa72f252d0c7d90e92
Richard Zou <zou3519@gmail.com>
2016-11-07 21:16:13 -0500

    Better clean script

--- clean.sh
+++ clean.sh
@@ -3,10 +3,12 @@
 
 if [ $# -eq 0 ]
 	then
+		echo "rm -rf GMLs content heatmaps models webgraphs"
 		rm -rf GMLs content heatmaps models webgraphs
 	else
 		for var in "$@"
 		do
-		    rm -rf "GMLs/$var.txt" "content/$var.txt" "heatmaps/$var_$var.html" "models/$var" "webgraphs/$var"
+			echo "rm -rf GMLs/$var.txt content/$var.txt heatmaps/${var}_$var.html models/$var.txt webgraphs/$var"
+		    rm -rf "GMLs/$var.txt" "content/$var.txt" "heatmaps/${var}_$var.html" "models/$var.txt" "webgraphs/$var"
 		done
 fi
